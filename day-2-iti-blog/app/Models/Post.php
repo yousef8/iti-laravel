d2f902f996bd72711e3b7e875667d87c0de87420
Yousef Elsayed <yousef4511@outlook.com>
2024-04-30 07:57:54 +0300

    Delete image on post deletion

--- day-2-iti-blog/app/Models/Post.php
+++ day-2-iti-blog/app/Models/Post.php
@@ -4,9 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'body', 'image'];
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Listen for the 'deleting' event
+        static::deleting(function ($post) {
+            // If the post has an associated image, delete the image file from storage
+            if ($post->image) {
+                Storage::disk('public')->delete('images/' . basename($post->image));
+            }
+        });
+    }
 }
