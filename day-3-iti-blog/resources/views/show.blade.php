0349fdba38bee47ba3413d9c2853a95b004ccee1
Yousef Elsayed <yousef4511@outlook.com>
2024-05-03 14:45:01 +0300

    Add control buttons to show page

--- day-3-iti-blog/resources/views/show.blade.php
+++ day-3-iti-blog/resources/views/show.blade.php
@@ -8,6 +8,12 @@
   <div class="card-body">
     <h5 class="card-title">{{$post->title}}</h5>
     <p class="card-text">{{$post->body}}</p>
+    <a href="{{route('posts.edit', $post->id)}}" ><x-button type="warning">Edit</x-button></a>
+    <form action="{{route('posts.destroy', $post->id)}}" method="POST">
+      @csrf
+      @method('delete')
+    <input type="submit" class="btn btn-danger" value='Delete' onclick="return confirm('Are you sure?')"/>
+  </form>
   </div>
 </div>
 @endsection
