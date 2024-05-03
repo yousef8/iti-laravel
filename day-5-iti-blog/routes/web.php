5a0893a26afe2fac3f29c3ffa9ce1dda2a7be7e0
Yousef Elsayed <yousef4511@outlook.com>
2024-05-03 23:37:25 +0300

    Create Post  migration, seeder, routes, model

--- day-5-iti-blog/routes/web.php
+++ day-5-iti-blog/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::apiResource('posts', PostController::class)->parameters(['posts' => 'id']);
